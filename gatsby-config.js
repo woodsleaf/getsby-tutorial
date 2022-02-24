622b5940e7233b176ffcac1af64403976b32209c
Pavel Kononov <woodsleaf@gmail.com>
2022-02-24 06:11:19 +0300

    add use env in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
@@ -15,5 +19,14 @@ module.exports = {
         },
         "gatsby-plugin-mdx",
         "gatsby-transformer-sharp",
-    ]
+        //{
+        //    resolve: "gatsby-source-pg",
+        //    options: {
+        //      connectionString: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`,
+        //      //connectionString: `${process.env.POSTGRES_CONNECT}`,
+        //      schema: "public",
+        //      refetchInterval: 60, // Refetch data every 60 seconds
+        //    },
+        //},
+    ],
 }
